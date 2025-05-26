ff98135dc023dd42c8d8b24a4bad5b52bd28b7c0
Rafael <rafaelokabe@gmail.com>
2025-05-26 11:20:19 -0300

    Initial commit

---
+++ Sea Survival Game/JS/game.js
@@ -0,0 +1,59 @@
+class mainScene {
+
+    //Like the awake in unty, it plays before the create (start) method
+    preload() {
+        //Uses to load assets
+        this.load.image('player', 'assets/peixe-lanterna.jpg')
+    }
+
+    create() {
+        this.player = this.physics.add.sprite(500, 500, 'player');
+
+        //Pontuação
+        this.score = 0;
+        let style = { font: '20px Arial', fill: "white" };
+        this.scoreText = this.add.text(50, 50, 'Score: ' + this.score, style);
+
+        //Adiciona input do teclado
+        this.arrow = this.input.keyboard.createCursorKeys();
+        this.leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+
+
+    }
+
+    update() {
+
+        //Movimentação horizontal
+        /*
+        if(this.leftKey.isDown){
+            //this.player.x -= 3; //Move para a esquerda
+            console.log('A');
+        }
+        */
+
+        
+        if (this.arrow.right.isDown) {
+            this.player.x += 3; //Move para a direita
+        } else if (this.arrow.left.isDown) {
+            this.player.x -= 3; //Move para a esquerda
+        }
+
+        //Movimentação vertical
+        if (this.arrow.down.isDown) {
+            this.player.y += 3; //Move para baixo
+        } else if (this.arrow.up.isDown) {
+            this.player.y -= 3; //Move para cima
+        }
+        
+    }
+
+}
+
+new Phaser.Game({
+    width: 700,
+    height: 500,
+    backgroundColor: '#040483',
+    scene: mainScene,
+    physics: { default: 'arcade' },
+    parent: 'game',
+});
